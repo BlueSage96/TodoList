b71a8c1a87412d9aa7922e59abe6ea3a80f85eac
Brittany Price <bprice0496@gmail.com>
2025-05-25 17:22:06 -0400

    Fixed cancel button

--- src/features/Todolist/TodoListItem.jsx
+++ src/features/Todolist/TodoListItem.jsx
@@ -38,7 +38,7 @@ function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
               onChange={handleEdit}
             />
             <button type="button" onClick={handleUpdate}>Update</button>
-            <button type="button" onChange={handleCancel}>Cancel</button>
+            <button type="button" onClick={handleCancel}>Cancel</button>
           </>
         ) : (
           /*
